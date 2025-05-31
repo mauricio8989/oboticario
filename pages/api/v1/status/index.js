ce92c8103d2c20dc51cafe9da639fc3c0644b4b9
mauricio89 <mauriciosouza8989@gmail.com>
2025-05-31 13:45:33 -0300

    add  and  scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-function status(req, res){
-    res.status(200).json({
-        status: "ok",
-        message: "API is running smoothly"
-    });
+function status(req, res) {
+  res.status(200).json({
+    status: "ok",
+    message: "API is running smoothly",
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,8 @@
 test("Get to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status")
-    expect(response.status).toBe(200);
-    expect(response.json()).toEqual({
-        status: "ok",
-        message: "API is running smoothly"
-    });
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+  expect(response.json()).toEqual({
+    status: "ok",
+    message: "API is running smoothly",
+  });
 });
