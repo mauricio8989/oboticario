b71d5662bf565683ca5edd31d3c95c94a5841227
mauricio89 <mauriciosouza8989@gmail.com>
2025-05-31 13:22:21 -0300

    updating repository

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem-vindo à Home</h1>
+            <p>Esta é a página inicial do seu aplicativo.</p>
+        </div>
+    );
+};
+
+export default Home;
